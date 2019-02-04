deda7c47d70b9619f6be4073011a93e6427cee50
Brian Overstreet <brian@pinterest.com>
2019-02-04 12:34:09 -0800

    wip

--- src/kayenta/reducers/opentsdbMetricConfig.ts
+++ src/kayenta/reducers/opentsdbMetricConfig.ts
@@ -2,27 +2,27 @@ import { Action } from 'redux';
 import { handleActions } from 'redux-actions';
 
 import * as Actions from 'kayenta/actions';
-// import { IUpdateKeyValueListPayload, updateListReducer } from '../layout/keyValueList';
+import { IUpdateKeyValueListPayload, updateListReducer } from '../layout/keyValueList';
 import { IOpentsdbCanaryMetricSetQueryConfig } from 'kayenta/metricStore/opentsdb/domain/IOpentsdbCanaryMetricSetQueryConfig';
-// import { IKayentaAction } from '../actions/creators';
+import { IKayentaAction } from '../actions/creators';
 import { ICanaryMetricConfig } from '../domain';
 
-// const updateTagPairsReducer = updateListReducer();
+const updateTagPairsReducer = updateListReducer();
 
 type IOpentsdbMetricConfig = ICanaryMetricConfig<IOpentsdbCanaryMetricSetQueryConfig>;
 
 export const opentsdbMetricConfigReducer = handleActions(
   {
-    // [Actions.UPDATE_OPENTSDB_TAG_PAIRS]: (
-    //   state: IOpentsdbMetricConfig,
-    //   action: IKayentaAction<IUpdateKeyValueListPayload>,
-    // ) => ({
-    //   ...state,
-    //   query: {
-    //     ...state.query,
-    //     tagPairs: updateTagPairsReducer(state.query.tagPairs || [], action),
-    //   },
-    // }),
+    [Actions.UPDATE_OPENTSDB_TAG_PAIRS]: (
+      state: IOpentsdbMetricConfig,
+      action: IKayentaAction<IUpdateKeyValueListPayload>,
+    ) => ({
+      ...state,
+      query: {
+        ...state.query,
+        tagPairs: updateTagPairsReducer(state.query.tagPairs || [], action),
+      },
+    }),
     [Actions.UPDATE_OPENTSDB_AGGREGATOR]: (state: IOpentsdbMetricConfig, action: Action & any) => ({
       ...state,
       query: { ...state.query, aggregator: action.payload.aggregator },
